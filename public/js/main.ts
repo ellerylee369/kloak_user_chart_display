f1b12e635effa5c21296c64e1aa37c7be9ca840c
Ellery <ellerywh@alumni.ubc.ca>
2020-05-31 22:16:02 -0700

    implemented fetch ai which feeds data to chart.js for graphing.  Issue currently being fixed

---
+++ public/js/main.ts
@@ -0,0 +1,133 @@
+const numbers_url = './numbers.txt'
+const dates_url = './dates.txt'
+declare var globalNumbers:number[]
+
+
+
+const getNumbers = () =>  { 
+    fetch(numbers_url).then(res => {
+         return res.text()
+     }).then(text => {
+         const numbers = JSON.parse(text)
+         console.log("FROM FETCH",numbers)
+         storeNumbers(numbers)
+ 
+         
+     })
+     }
+ 
+const storeNumbers = (numbers) => {
+     globalNumbers = numbers
+     console.log(globalNumbers)
+     
+     
+     
+ 
+ }
+
+
+
+
+
+/*const getNumbers = () => { 
+   return fetch(numbers_url).then(res => {
+        return res.text()
+    }).then(text => {
+        const numbers = JSON.parse(text)
+        console.log("FROM FETCH",numbers)
+        return numbers
+
+        
+    })
+    }
+
+const storeNumbers = async () => {
+    const globalNumbers = console.log(await getNumbers())
+    displayGraph(globalNumbers)
+    
+
+}*/
+
+
+
+
+
+
+
+
+
+//.then() is passing the data into the NEXT function
+
+/*getNumbersandDates = () => {
+    Promise.all([
+        fetch(numbers_url),
+        fetch(dates_url)
+
+    ]).then((res) => {
+        return res.map(function(res)
+        {return res.text()})
+    }).then((text) => {
+        console.log('from promise.all', text)
+        const [numbers, dates] = text
+        //const graph_numbers = JSON.parse(numbers)
+        //const graph_dates = JSON.parse(dates)
+        console.log(graph_numbers)
+        console.log(graph_dates)
+        //displayGraph(graph_numbers,graph_dates)
+    })
+}*/
+
+
+const displayGraph = (graphnumbers) => {
+        
+        console.log("FROM GRAPH", graphnumbers)
+        const canvas = <HTMLCanvasElement>document.getElementById('line-chart')
+        const ctx =canvas.getContext('2d')
+        new Chart(ctx, {
+            type: 'line',
+            data : {
+                labels: ['May 1', 'May 2', 'May 3', 'May 4', 'May 5'],
+                datasets: [{
+                fill: false,
+                borderColor: 'rgb(255,99,132)',
+                data : graphnumbers
+            }]
+        },
+            options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Kloak platform total users'
+                },
+                scales : {
+                    xAxes : [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes : [{
+                        ticks: {
+                            min: 0,
+                            max: 100,
+                            stepSize: 10
+                        },
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Number of total users'
+                        },
+                    },
+                    ],
+
+                },
+            },
+
+        });
+    }
+    
+getNumbers()
+const graphnumbers = globalNumbers
+displayGraph(graphnumbers)
+
